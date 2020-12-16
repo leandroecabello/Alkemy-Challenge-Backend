4a81e100ec987cf15933c035a5f34e9ec9134fda
leandro <leandro.ecabello@gmail.com>
2020-12-16 20:09:08 -0300

    Create controllers

--- src/app/controllers/post.controller.js
+++ src/app/controllers/post.controller.js
@@ -4,9 +4,9 @@ const postCtrl = {};
 
 postCtrl.getAll = async (req, res) => {
   try {
-    const operations = await postService.getAll();
-    console.log(operations);
-    res.status(200).json(operations);
+    const posts = await postService.getAll();
+    console.log(posts);
+    res.status(200).json(posts);
   } catch (err) {
     console.log(err);
     res.status(500).json({
@@ -15,3 +15,27 @@ postCtrl.getAll = async (req, res) => {
     });
   }
 };
+
+postCtrl.addPost = async (req, res) => {
+  const { title, content, image, category } = req.body;
+
+  try {
+    const operation = await postService.store({
+      title,
+      content,
+      image,
+      category,
+    });
+    res
+      .status(201)
+      .json({ message: 'Operation created successfully', operation });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: 'Something went wrong. Please retry or contact with an admin.',
+      message: err,
+    });
+  }
+};
+
+module.exports = postCtrl;
