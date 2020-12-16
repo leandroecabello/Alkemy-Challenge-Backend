faddf4ec45ae7908da23787f309b09a1b16ac0ce
leandro <leandro.ecabello@gmail.com>
2020-12-16 17:51:23 -0300

    Configure db with sequelize

---
+++ src/database.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+const { DDBB } = require('./config/config');
+
+const dbQueryString = `mysql://${DDBB.USER}:${DDBB.PASS}@${DDBB.HOST}:${DDBB.PORT}/${DDBB.NAME}`;
+const sequelize = new Sequelize(dbQueryString);
+
+const db = {};
+
+db.dbConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection has been established successfully');
+  } catch (err) {
+    console.error('Unable to connect to the database:', err);
+  }
+};
+
+db.dbSyncTables = async () => {
+  await sequelize.sync({ force: false });
+  console.log('Synchronized tables');
+};
+
+module.exports = { db, sequelize };
