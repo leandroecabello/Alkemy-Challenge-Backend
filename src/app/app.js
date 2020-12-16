1da5cc5e0968ad43253ee4079c1a85ba0be524ca
leandro <leandro.ecabello@gmail.com>
2020-12-16 20:45:38 -0300

    Update app file

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const postRoutes = require('./routes/post.routes');
 
 // Initialize
 const app = express();
@@ -14,5 +15,7 @@ app
   .use(cors())
   .use(express.json())
   .use(express.urlencoded({ extended: false }));
+
+app.use('/api/v1/posts', postRoutes);
 
 module.exports = app;
