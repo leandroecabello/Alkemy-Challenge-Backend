42b04d9e2777b643c4786bdfac88ae2ee3589072
leandro <leandro.ecabello@gmail.com>
2020-12-16 16:59:12 -0300

    Create config file

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+const config = {
+  DDBB: {
+    NAME: process.env.DDBB_NAME,
+    USER: process.env.DDBB_USER,
+    PASS: process.env.DDBB_PASS,
+    PORT: process.env.DDBB_PORT,
+    HOST: process.env.DDBB_HOST,
+  },
+};
+
+module.exports = config;
