2005e6e1191eb69977d151070c9556559e0b2987
leandro <leandro.ecabello@gmail.com>
2020-12-16 22:14:16 -0300

    Modifie database file

--- src/database.js
+++ src/database.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize');
 const { DDBB } = require('./config/config');
 
-const PostModel = require('./app/models/Post');
+// const PostModel = require('./app/models/Post');
 
 const dbQueryString = `mysql://${DDBB.USER}:${DDBB.PASS}@${DDBB.HOST}:${DDBB.PORT}/${DDBB.NAME}`;
 const sequelize = new Sequelize(dbQueryString);
-const Post = PostModel(sequelize, Sequelize);
+// const Post = PostModel(sequelize, Sequelize);
 
 const db = {};
 
@@ -23,4 +23,4 @@ db.dbSyncTables = async () => {
   console.log('Synchronized tables');
 };
 
-module.exports = { db, sequelize, Post };
+module.exports = { db, sequelize };
