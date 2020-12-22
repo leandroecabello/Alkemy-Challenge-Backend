bf0c0dca9608af430375322933b2e7800200c5c3
leandro <leandro.ecabello@gmail.com>
2020-12-22 05:52:41 -0300

    Add variables to config

--- src/config/config.js
+++ src/config/config.js
@@ -6,6 +6,14 @@ const config = {
     PORT: process.env.DDBB_PORT,
     HOST: process.env.DDBB_HOST,
   },
+
+  DEFAULT_PORT: 4000,
+  MORGAN_DEV: 'dev',
+
+  HTTP_CODE: {
+    CREATE: 201,
+    OK: 200,
+  },
 };
 
 module.exports = config;
