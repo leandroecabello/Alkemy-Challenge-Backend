5a8636e26636931037eeaacf055a123615c55302
leandro <leandro.ecabello@gmail.com>
2020-12-22 05:48:29 -0300

    Add Handle Errors

--- src/app.js
+++ src/app.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const boom = require('@hapi/boom');
+const errorHandler = require('./middlewares/errorHandler');
+const verifyBoom = require('./middlewares/verifyBoom');
+const { DEFAULT_PORT, MORGAN_DEV } = require('./config/config');
 const postRoutes = require('./routes/post.routes');
 
 const app = express();
 
-const defaultPort = 4000;
+const defaultPort = DEFAULT_PORT;
 app.set('port', process.env.PORT || defaultPort);
 
 app
-  .use(morgan('dev'))
+  .use(morgan(MORGAN_DEV))
   .use(cors())
   .use(express.json())
   .use(express.urlencoded({ extended: false }));
 
-app.use('/api/v1/posts', postRoutes);
+app
+  .use('/posts', postRoutes)
+  .use('*', (req, res, next) => next(boom.notFound()));
+
+app.use(verifyBoom);
+app.use(errorHandler);
 
 module.exports = app;
