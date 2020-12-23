a847c0284f61bd8990d83656b43464fb187d1ce1
leandro <leandro.ecabello@gmail.com>
2020-12-23 09:21:07 -0300

    Add DbTest in config file

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,7 @@
 const config = {
+  DEFAULT_PORT: 4000,
+  MORGAN_DEV: 'dev',
+
   DDBB: {
     NAME: process.env.DDBB_NAME,
     USER: process.env.DDBB_USER,
@@ -7,8 +10,13 @@ const config = {
     HOST: process.env.DDBB_HOST,
   },
 
-  DEFAULT_PORT: 4000,
-  MORGAN_DEV: 'dev',
+  DB_TEST: {
+    NAME: 'alkemy-blog',
+    USER: 'root',
+    PASS: '',
+    PORT: '3306',
+    HOST: '127.0.0.1',
+  },
 
   HTTP_CODE: {
     CREATE: 201,
