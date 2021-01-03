5782936318cde73b9e6985d7e840428d620ed471
leandro <leandro.ecabello@gmail.com>
2021-01-03 14:00:52 -0300

    Modify dbQueryString and object db

--- src/database.js
+++ src/database.js
@@ -1,25 +1,25 @@
 const Sequelize = require('sequelize');
-const { DDBB } = require('./config/config');
+const { DDBB, DB_TEST } = require('./config/config');
 
-const { USER, PASS, HOST, PORT, NAME } = DDBB;
+const { USER, PASS, HOST, PORT, NAME, DIALECT } = DDBB || DB_TEST;
 
-const dbQueryString = `mysql://${USER}:${PASS}@${HOST}:${PORT}/${NAME}`;
+const dbQueryString = `${DIALECT}://${USER}:${PASS}@${HOST}:${PORT}/${NAME}`;
 const sequelize = new Sequelize(dbQueryString);
 
-const db = {};
+const db = {
+  dbConnection: async () => {
+    try {
+      await sequelize.authenticate();
+      console.log('Database connection has been established successfully');
+    } catch (err) {
+      console.error('Unable to connect to the database:', err);
+    }
+  },
 
-db.dbConnection = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Database connection has been established successfully');
-  } catch (err) {
-    console.error('Unable to connect to the database:', err);
-  }
-};
-
-db.dbSyncTables = async () => {
-  await sequelize.sync({ force: false });
-  console.log('Synchronized tables');
+  dbSyncTables: async () => {
+    await sequelize.sync({ force: false });
+    console.log('Synchronized tables');
+  },
 };
 
 module.exports = { db, sequelize };
